264d61865b5e216b7d2b62cb99e8870f53878161
hongyi1024 <381359559@qq.com>
2022-05-28 17:17:10 +0800

    新建项目

---
+++ Mobile/app/src/main/java/com/example/mechanical_industrial_goods_eommerce_project_for_android/Utils/ToastUtils.java
@@ -0,0 +1,18 @@
+package com.example.mechanical_industrial_goods_eommerce_project_for_android.Utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * 消息提示工具类
+ */
+public class ToastUtils {
+    public static void showLongToast(Context context, CharSequence charSequence) {
+        //应用context,显示的文本，消息框停留的市场
+        Toast.makeText(context.getApplicationContext(), charSequence, Toast.LENGTH_LONG).show();
+    }
+
+    public static void showShortToast(Context context, CharSequence charSequence) {
+        Toast.makeText(context.getApplicationContext(), charSequence, Toast.LENGTH_SHORT).show();
+    }
+}
