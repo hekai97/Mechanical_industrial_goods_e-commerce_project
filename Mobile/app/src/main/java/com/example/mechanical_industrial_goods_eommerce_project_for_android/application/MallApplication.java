5db4c197085d53379e3cef49a56ff3b582005894
hongyi1024 <381359559@qq.com>
2022-06-03 21:49:48 +0800

    移动端购物车页和用户中心页跳转登录页面完成，实现用户会话保存

--- Mobile/app/src/main/java/com/example/mechanical_industrial_goods_eommerce_project_for_android/application/MallApplication.java
+++ Mobile/app/src/main/java/com/example/mechanical_industrial_goods_eommerce_project_for_android/application/MallApplication.java
@@ -2,19 +2,35 @@ package com.example.mechanical_industrial_goods_eommerce_project_for_android.app
 
 import android.app.Application;
 
+import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.cookie.CookieJarImpl;
+import com.zhy.http.okhttp.cookie.store.PersistentCookieStore;
+import com.zhy.http.okhttp.log.LoggerInterceptor;
+
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.CookieJar;
+import okhttp3.OkHttpClient;
 
 public class MallApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        initOkHttpUtils();
     }
 
     private void initOkHttpUtils(){
 
+        //保持Cookie
+        CookieJarImpl cookieJar = new CookieJarImpl(new PersistentCookieStore(getApplicationContext()));
+        OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
+                .cookieJar(cookieJar)
+                .connectTimeout(10000L, TimeUnit.MILLISECONDS)
+                .readTimeout(10000L, TimeUnit.MILLISECONDS)
+                .addInterceptor(new LoggerInterceptor("TAG"))
+                .build();
+        OkHttpUtils.initClient(okHttpClient);
 
     }
 }
